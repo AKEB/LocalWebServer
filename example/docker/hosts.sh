fa0782b608351dec3da4ca04bdd0ce6f0b60f279
Vadim Babajanyan <babadzhanyan@corp.mail.ru>
2023-08-18 01:40:32 +0300

    Example usage project

---
+++ example/docker/hosts.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# vim:sw=4:ts=4:et
+
+IP=`getent hosts host.docker.internal | awk '{print $1}'`
+
+echo "IP=${IP}"
+
+echo "${IP} my.example.ru" >> /etc/hosts
+getent hosts nginx >> /etc/hosts
+
+exit 0
