8c6f0ea1782bf9e01e9ebf3ab0c4d831ab2f37c4
Vadim Babadzhanyan <vadim.babadzhanyan@my.games>
2024-04-19 15:49:39 +0300

    arm64

--- build_docker_php_fpm.sh
+++ build_docker_php_fpm.sh
@@ -1,10 +1,49 @@
 #!/bin/bash
 
-cp ssl/root_ca.crt nginx/ && docker build -t babadzhanyan/nginx:latest ./nginx/ --push
+# Nginx
+cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx/ && docker buildx build  \
+--push \
+--platform linux/386,linux/amd64,linux/arm/v5,linux/arm/v7,linux/arm64,linux/mips64le,linux/ppc64le,linux/s390x \
+--tag babadzhanyan/nginx:latest ${PWD}/nginx/
 
-cp ssl/root_ca.crt php-fpm-7_2/ && docker build -t babadzhanyan/php-fpm:7.2 ./php-fpm-7_2/ --push
-cp ssl/root_ca.crt php-fpm-7_3/ && docker build -t babadzhanyan/php-fpm:7.3 ./php-fpm-7_3/ --push
-cp ssl/root_ca.crt php-fpm-7_4/ && docker build -t babadzhanyan/php-fpm:7.4 ./php-fpm-7_4/ --push
-cp ssl/root_ca.crt php-fpm-8_0/ && docker build -t babadzhanyan/php-fpm:8.0 ./php-fpm-8_0/ --push
-cp ssl/root_ca.crt php-fpm-8_1/ && docker build -t babadzhanyan/php-fpm:8.1 ./php-fpm-8_1/ --push
-cp ssl/root_ca.crt php-fpm-8_2/ && docker build -t babadzhanyan/php-fpm:8.2 ./php-fpm-8_2/ --push
+# PHP 7.2
+cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-7_2/ && docker buildx build \
+--push \
+--platform linux/amd64,linux/arm/v7,linux/arm64/v8 \
+--tag babadzhanyan/php-fpm:7.2 ${PWD}/php-fpm-7_2/ 
+
+# PHP 7.3
+cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-7_3/ && docker buildx build \
+--push \
+--platform linux/amd64,linux/arm/v7,linux/arm64/v8 \
+--tag babadzhanyan/php-fpm:7.3 ${PWD}/php-fpm-7_3/ 
+
+# PHP 7.4
+cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-7_4/ && docker buildx build \
+--push \
+--platform linux/amd64,linux/arm/v7,linux/arm64/v8 \
+--tag babadzhanyan/php-fpm:7.4 ${PWD}/php-fpm-7_4/
+
+# PHP 8.0
+cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-8_0/ && docker buildx build \
+--push \
+--platform linux/amd64,linux/arm/v7,linux/arm64/v8 \
+--tag babadzhanyan/php-fpm:8.0 ${PWD}/php-fpm-8_0/
+
+# PHP 8.1
+cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-8_1/ && docker buildx build \
+--push \
+--platform linux/amd64,linux/arm/v7,linux/arm64/v8 \
+--tag babadzhanyan/php-fpm:8.1 ${PWD}/php-fpm-8_1/
+
+# PHP 8.2
+cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-8_2/ && docker buildx build \
+--push \
+--platform linux/amd64,linux/arm/v7,linux/arm64/v8 \
+--tag babadzhanyan/php-fpm:8.2 ${PWD}/php-fpm-8_2/
+
+# PHP 8.3
+cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-8_3/ && docker buildx build \
+--push \
+--platform linux/amd64,linux/arm/v7,linux/arm64/v8 \
+--tag babadzhanyan/php-fpm:8.3 ${PWD}/php-fpm-8_3/
