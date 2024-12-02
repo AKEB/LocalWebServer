c029bc7658b9866e26999dd13e568357a7dd2948
Vadim Babadzhanyan <vadim.babadzhanyan@my.games>
2024-12-02 18:12:56 +0300

    Change docker hub account

--- build_docker_php_fpm.sh
+++ build_docker_php_fpm.sh
@@ -20,12 +20,12 @@ echo "Starting buildings..."
 # --push \
 # --platform linux/386,linux/amd64,linux/arm/v5,linux/arm/v7,linux/arm64,linux/mips64le,linux/ppc64le,linux/s390x \
 docker build \
-  --tag babadzhanyan/nginx:latest \
+  --tag akeb/nginx:latest \
   ${PWD}/nginx/ > ${PWD}/build/nginx.log 2>&1 &
 
 # # PHP 7.1
 docker build \
-  --tag babadzhanyan/php-fpm:7.1 \
+  --tag akeb/php-fpm:7.1 \
   ${PWD}/php-fpm-7_1/ > ${PWD}/build/php-fpm-7_1.log 2>&1 &
 
 
@@ -34,37 +34,37 @@ docker build \
 # --platform linux/amd64,linux/arm/v7,linux/arm64/v8 \
 
 docker build \
-  --tag babadzhanyan/php-fpm:7.2 \
+  --tag akeb/php-fpm:7.2 \
   ${PWD}/php-fpm-7_2/ > ${PWD}/build/php-fpm-7_2.log 2>&1 &
 
 # PHP 7.3
 docker build \
-  --tag babadzhanyan/php-fpm:7.3 \
+  --tag akeb/php-fpm:7.3 \
   ${PWD}/php-fpm-7_3/ > ${PWD}/build/php-fpm-7_3.log 2>&1 &
 
 # PHP 7.4
 docker build \
-  --tag babadzhanyan/php-fpm:7.4 \
+  --tag akeb/php-fpm:7.4 \
   ${PWD}/php-fpm-7_4/ > ${PWD}/build/php-fpm-7_4.log 2>&1 &
 
 # PHP 8.0
 docker build \
-  --tag babadzhanyan/php-fpm:8.0 \
+  --tag akeb/php-fpm:8.0 \
   ${PWD}/php-fpm-8_0/ > ${PWD}/build/php-fpm-8_0.log 2>&1 &
 
 # PHP 8.1
 docker build \
-  --tag babadzhanyan/php-fpm:8.1 \
+  --tag akeb/php-fpm:8.1 \
   ${PWD}/php-fpm-8_1/ > ${PWD}/build/php-fpm-8_1.log 2>&1 &
 
 # PHP 8.2
 docker build \
-  --tag babadzhanyan/php-fpm:8.2 \
+  --tag akeb/php-fpm:8.2 \
   ${PWD}/php-fpm-8_2/ > ${PWD}/build/php-fpm-8_2.log 2>&1 &
 
 # PHP 8.3
 docker build \
-  --tag babadzhanyan/php-fpm:8.3 \
+  --tag akeb/php-fpm:8.3 \
   ${PWD}/php-fpm-8_3/ > ${PWD}/build/php-fpm-8_3.log 2>&1 &
 
 
