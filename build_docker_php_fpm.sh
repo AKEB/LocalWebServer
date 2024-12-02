f756ac76564ded55f9d52f7afdefb94396699bc1
Vadim Babadzhanyan <vadim.babadzhanyan@my.games>
2024-12-02 19:51:13 +0300

    New Docker Image

--- build_docker_php_fpm.sh
+++ build_docker_php_fpm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx/
+
+cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-7_1/
 cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-7_2/
 cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-7_3/
 cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-7_4/
@@ -8,6 +10,15 @@ cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-8_0/
 cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-8_1/
 cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-8_2/
 cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-8_3/
+
+cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-7_1/
+cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-7_2/
+cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-7_3/
+cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-7_4/
+cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-8_0/
+cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-8_1/
+cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-8_2/
+cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-8_3/
 
 rm -rf ${PWD}/build/*.*
 mkdir -p ${PWD}/build/
@@ -23,16 +34,13 @@ docker build \
   --tag akeb/nginx:latest \
   ${PWD}/nginx/ > ${PWD}/build/nginx.log 2>&1 &
 
-# # PHP 7.1
+# PHP 7.1
 docker build \
   --tag akeb/php-fpm:7.1 \
   ${PWD}/php-fpm-7_1/ > ${PWD}/build/php-fpm-7_1.log 2>&1 &
 
 
-# # PHP 7.2
-# --push \
-# --platform linux/amd64,linux/arm/v7,linux/arm64/v8 \
-
+# PHP 7.2
 docker build \
   --tag akeb/php-fpm:7.2 \
   ${PWD}/php-fpm-7_2/ > ${PWD}/build/php-fpm-7_2.log 2>&1 &
@@ -66,6 +74,52 @@ docker build \
 docker build \
   --tag akeb/php-fpm:8.3 \
   ${PWD}/php-fpm-8_3/ > ${PWD}/build/php-fpm-8_3.log 2>&1 &
+
+
+
+
+# NGINX PHP 7.1
+docker build \
+  --tag akeb/nginx-php-fpm:7.1 \
+  ${PWD}/nginx-php-fpm-7_1/ > ${PWD}/build/nginx-php-fpm-7_1.log 2>&1 &
+
+
+# NGINX PHP 7.2
+docker build \
+  --tag akeb/nginx-php-fpm:7.2 \
+  ${PWD}/nginx-php-fpm-7_2/ > ${PWD}/build/nginx-nginx-php-fpm-7_2.log 2>&1 &
+
+# NGINX PHP 7.3
+docker build \
+  --tag akeb/nginx-php-fpm:7.3 \
+  ${PWD}/nginx-php-fpm-7_3/ > ${PWD}/build/nginx-php-fpm-7_3.log 2>&1 &
+
+# NGINX PHP 7.4
+docker build \
+  --tag akeb/nginx-php-fpm:7.4 \
+  ${PWD}/nginx-php-fpm-7_4/ > ${PWD}/build/nginx-php-fpm-7_4.log 2>&1 &
+
+# NGINX PHP 8.0
+docker build \
+  --tag akeb/nginx-php-fpm:8.0 \
+  ${PWD}/nginx-php-fpm-8_0/ > ${PWD}/build/nginx-php-fpm-8_0.log 2>&1 &
+
+# NGINX PHP 8.1
+docker build \
+  --tag akeb/nginx-php-fpm:8.1 \
+  ${PWD}/nginx-php-fpm-8_1/ > ${PWD}/build/nginx-php-fpm-8_1.log 2>&1 &
+
+# NGINX PHP 8.2
+docker build \
+  --tag akeb/nginx-php-fpm:8.2 \
+  ${PWD}/nginx-php-fpm-8_2/ > ${PWD}/build/nginx-php-fpm-8_2.log 2>&1 &
+
+# NGINX PHP 8.3
+docker build \
+  --tag akeb/nginx-php-fpm:8.3 \
+  ${PWD}/nginx-php-fpm-8_3/ > ${PWD}/build/nginx-php-fpm-8_3.log 2>&1 &
+
+
 
 
 for job in `jobs -p`
