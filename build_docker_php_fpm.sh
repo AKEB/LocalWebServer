2a5353c5354481cddfbc26bc858bc6c15a5561c9
Vadim Babadzhanyan <vadim.babadzhanyan@my.games>
2024-12-18 16:05:34 +0300

    Big Fix

--- build_docker_php_fpm.sh
+++ build_docker_php_fpm.sh
@@ -2,23 +2,8 @@
 
 cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx/
 
-cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-7_1/
-cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-7_2/
-cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-7_3/
-cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-7_4/
-cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-8_0/
-cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-8_1/
-cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-8_2/
-cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm-8_3/
-
-cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-7_1/
-cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-7_2/
-cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-7_3/
-cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-7_4/
-cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-8_0/
-cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-8_1/
-cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-8_2/
-cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm-8_3/
+cp ${PWD}/ssl/root_ca.crt ${PWD}/php-fpm/
+cp ${PWD}/ssl/root_ca.crt ${PWD}/nginx-php-fpm/
 
 rm -rf ${PWD}/build/*.*
 mkdir -p ${PWD}/build/
@@ -37,88 +22,102 @@ echo "Starting buildings..."
 # # PHP 7.1
 # docker build \
 #   --tag akeb/php-fpm:7.1 \
-#   ${PWD}/php-fpm-7_1/ > ${PWD}/build/php-fpm-7_1.log 2>&1 &
-
+#   --build-arg PHP_VERSION=7.1 \
+#   ${PWD}/php-fpm/ > ${PWD}/build/php-fpm-7_1.log 2>&1 &
 
 # # PHP 7.2
 # docker build \
 #   --tag akeb/php-fpm:7.2 \
-#   ${PWD}/php-fpm-7_2/ > ${PWD}/build/php-fpm-7_2.log 2>&1 &
+#   --build-arg PHP_VERSION=7.2 \
+#   ${PWD}/php-fpm/ > ${PWD}/build/php-fpm-7_2.log 2>&1 &
 
 # # PHP 7.3
 # docker build \
 #   --tag akeb/php-fpm:7.3 \
-#   ${PWD}/php-fpm-7_3/ > ${PWD}/build/php-fpm-7_3.log 2>&1 &
+#   --build-arg PHP_VERSION=7.3 \
+#   ${PWD}/php-fpm/ > ${PWD}/build/php-fpm-7_3.log 2>&1 &
 
 # # PHP 7.4
 # docker build \
 #   --tag akeb/php-fpm:7.4 \
-#   ${PWD}/php-fpm-7_4/ > ${PWD}/build/php-fpm-7_4.log 2>&1 &
+#   --build-arg PHP_VERSION=7.4 \
+#   ${PWD}/php-fpm/ > ${PWD}/build/php-fpm-7_4.log 2>&1 &
+
 
 # # PHP 8.0
 # docker build \
 #   --tag akeb/php-fpm:8.0 \
-#   ${PWD}/php-fpm-8_0/ > ${PWD}/build/php-fpm-8_0.log 2>&1 &
+#   --build-arg PHP_VERSION=8.0 \
+#   ${PWD}/php-fpm/ > ${PWD}/build/php-fpm-8_0.log 2>&1 &
 
 # # PHP 8.1
 # docker build \
 #   --tag akeb/php-fpm:8.1 \
-#   ${PWD}/php-fpm-8_1/ > ${PWD}/build/php-fpm-8_1.log 2>&1 &
+#   --build-arg PHP_VERSION=8.1 \
+#   ${PWD}/php-fpm/ > ${PWD}/build/php-fpm-8_1.log 2>&1 &
 
 # # PHP 8.2
 # docker build \
 #   --tag akeb/php-fpm:8.2 \
-#   ${PWD}/php-fpm-8_2/ > ${PWD}/build/php-fpm-8_2.log 2>&1 &
+#   --build-arg PHP_VERSION=8.2 \
+#   ${PWD}/php-fpm/ > ${PWD}/build/php-fpm-8_2.log 2>&1 &
 
 # # PHP 8.3
 # docker build \
 #   --tag akeb/php-fpm:8.3 \
-#   ${PWD}/php-fpm-8_3/ > ${PWD}/build/php-fpm-8_3.log 2>&1 &
+#   --build-arg PHP_VERSION=8.3 \
+#   ${PWD}/php-fpm/ > ${PWD}/build/php-fpm-8_3.log 2>&1 &
 
 
-
-
-# # NGINX PHP 7.1
+# NGINX PHP 7.1
 # docker build \
 #   --tag akeb/nginx-php-fpm:7.1 \
-#   ${PWD}/nginx-php-fpm-7_1/ > ${PWD}/build/nginx-php-fpm-7_1.log 2>&1 &
+#   --build-arg PHP_VERSION=7.1 \
+#   ${PWD}/nginx-php-fpm/ > ${PWD}/build/nginx-php-fpm-7_1.log 2>&1 &
 
-
-# # NGINX PHP 7.2
+# NGINX PHP 7.2
 # docker build \
 #   --tag akeb/nginx-php-fpm:7.2 \
-#   ${PWD}/nginx-php-fpm-7_2/ > ${PWD}/build/nginx-php-fpm-7_2.log 2>&1 &
+#   --build-arg PHP_VERSION=7.2 \
+#   ${PWD}/nginx-php-fpm/ > ${PWD}/build/nginx-php-fpm-7_2.log 2>&1 &
 
-# # NGINX PHP 7.3
+# NGINX PHP 7.3
 # docker build \
 #   --tag akeb/nginx-php-fpm:7.3 \
-#   ${PWD}/nginx-php-fpm-7_3/ > ${PWD}/build/nginx-php-fpm-7_3.log 2>&1 &
+#   --build-arg PHP_VERSION=7.3 \
+#   ${PWD}/nginx-php-fpm/ > ${PWD}/build/nginx-php-fpm-7_3.log 2>&1 &
 
-# # NGINX PHP 7.4
+# NGINX PHP 7.4
 # docker build \
 #   --tag akeb/nginx-php-fpm:7.4 \
-#   ${PWD}/nginx-php-fpm-7_4/ > ${PWD}/build/nginx-php-fpm-7_4.log 2>&1 &
+#   --build-arg PHP_VERSION=7.4 \
+#   ${PWD}/nginx-php-fpm/ > ${PWD}/build/nginx-php-fpm-7_4.log 2>&1 &
 
-# # NGINX PHP 8.0
+# NGINX PHP 8.0
 # docker build \
 #   --tag akeb/nginx-php-fpm:8.0 \
-#   ${PWD}/nginx-php-fpm-8_0/ > ${PWD}/build/nginx-php-fpm-8_0.log 2>&1 &
+#   --build-arg PHP_VERSION=8.0 \
+#   ${PWD}/nginx-php-fpm/ > ${PWD}/build/nginx-php-fpm-8_0.log 2>&1 &
 
-# # NGINX PHP 8.1
+
+# NGINX PHP 8.1
 # docker build \
 #   --tag akeb/nginx-php-fpm:8.1 \
-#   ${PWD}/nginx-php-fpm-8_1/ > ${PWD}/build/nginx-php-fpm-8_1.log 2>&1 &
+#   --build-arg PHP_VERSION=8.1 \
+#   ${PWD}/nginx-php-fpm/ > ${PWD}/build/nginx-php-fpm-8_1.log 2>&1 &
 
-# # NGINX PHP 8.2
+
+# NGINX PHP 8.2
 # docker build \
 #   --tag akeb/nginx-php-fpm:8.2 \
-#   ${PWD}/nginx-php-fpm-8_2/ > ${PWD}/build/nginx-php-fpm-8_2.log 2>&1 &
+#   --build-arg PHP_VERSION=8.2 \
+#   ${PWD}/nginx-php-fpm/ > ${PWD}/build/nginx-php-fpm-8_2.log 2>&1 &
 
 # NGINX PHP 8.3
 docker build \
   --tag akeb/nginx-php-fpm:8.3 \
-  ${PWD}/nginx-php-fpm-8_3/ > ${PWD}/build/nginx-php-fpm-8_3.log 2>&1 &
-
+  --build-arg PHP_VERSION=8.3 \
+  ${PWD}/nginx-php-fpm/ > ${PWD}/build/nginx-php-fpm-8_3.log 2>&1 &
 
 
 
